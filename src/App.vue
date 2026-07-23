c1812a1dd6cbc10742406717ae67009ea1587477
Jiyeon <jjiyeon27@gmail.com>
2026-07-23 17:44:52 +0900

    fix: 라우트 meta 기반 레이아웃 스위칭 적용

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <script setup>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+import DefaultLayout from '@/layouts/DefaultLayout.vue';
+import AuthLayout from '@/layouts/AuthLayout.vue';
+
+const route = useRoute();
+
+const layouts = {
+  DefaultLayout,
+  AuthLayout,
+};
+
+const layout = computed(
+  () => layouts[route.meta.layout] ?? null,
+);
 </script>
 
 <template>
-  <router-view />
+  <component :is="layout" v-if="layout" />
+  <router-view v-else />
 </template>
