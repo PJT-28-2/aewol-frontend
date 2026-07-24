a4d4938b314ba0b38391263e3b851cf498861ce5
Jiyeon <jjiyeon27@gmail.com>
2026-07-24 13:09:57 +0900

    feat: 홈 화면 구현

--- src/components/common/BottomNavBar.vue
+++ src/components/common/BottomNavBar.vue
@@ -21,7 +21,7 @@ function isActive(path) {
 
 <template>
   <nav
-    class="fixed bottom-0 left-0 right-0 z-[100] flex items-center justify-around h-[var(--bottom-nav-height)] bg-white border-t border-[#EAEAE4] pb-[env(safe-area-inset-bottom,0)]"
+    class="fixed bottom-0 left-0 right-0 z-100 flex items-center justify-around h-(--bottom-nav-height) bg-white border-t border-[#EAEAE4] pb-[env(safe-area-inset-bottom,0)]"
   >
     <router-link
       v-for="tab in tabs"
