233394e8f987aa38ce2b5338900295e11fe62847
Jiyeon <jjiyeon27@gmail.com>
2026-08-04 11:34:13 +0900

    refactor: 사용하지 않는 지갑 버킷 관련 API/스토어 코드 제거

--- src/api/wallet.js
+++ src/api/wallet.js
@@ -9,24 +9,4 @@ export const walletApi = {
   charge(amount) {
     return api.post('/wallet/deposit', null, { params: { amount } })
   },
-
-  getBuckets() {
-    return api.get('/wallet/buckets')
-  },
-
-  createBucket(data) {
-    return api.post('/wallet/buckets', data)
-  },
-
-  updateBucket(id, data) {
-    return api.put(`/wallet/buckets/${id}`, data)
-  },
-
-  deleteBucket(id) {
-    return api.delete(`/wallet/buckets/${id}`)
-  },
-
-  transferBucket(fromBucketId, toBucketId, amount) {
-    return api.post('/wallet/buckets/transfer', { fromBucketId, toBucketId, amount })
-  },
 }
--- src/stores/wallet.js
+++ src/stores/wallet.js
@@ -4,7 +4,6 @@ import { walletApi } from '@/api/wallet'
 export const useWalletStore = defineStore('wallet', {
   state: () => ({
     wallet: null,
-    buckets: [],
   }),
 
   actions: {
@@ -15,40 +14,11 @@ export const useWalletStore = defineStore('wallet', {
       return this.wallet
     },
 
-    async fetchBuckets() {
-      const { data } = await walletApi.getBuckets()
-      this.buckets = data
-      return data
-    },
-
-    async createBucket(bucketData) {
-      const { data } = await walletApi.createBucket(bucketData)
-      this.buckets.push(data)
-      return data
-    },
-
-    async updateBucket(id, bucketData) {
-      const { data } = await walletApi.updateBucket(id, bucketData)
-      const index = this.buckets.findIndex((b) => b.id === id)
-      if (index !== -1) this.buckets[index] = data
-      return data
-    },
-
-    async deleteBucket(id) {
-      await walletApi.deleteBucket(id)
-      this.buckets = this.buckets.filter((b) => b.id !== id)
-    },
-
     async charge(amount) {
       const { data } = await walletApi.charge(amount)
       // 백엔드 WalletResponse 필드명(totalBalance) 기준
       if (this.wallet) this.wallet.totalBalance = data.totalBalance
       else this.wallet = data
-      return data
-    },
-
-    async transferBucket(fromBucketId, toBucketId, amount) {
-      const { data } = await walletApi.transferBucket(fromBucketId, toBucketId, amount)
       return data
     },
   },
